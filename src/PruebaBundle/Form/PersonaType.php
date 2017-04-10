ed2b417e4c7d2e25613d56548bb94211c1661ee6
Fabiola <alexfabiola28@gmail.com>
2017-04-10 19:39:36 -0400

    CRUD Persona

---
+++ src/PruebaBundle/Form/PersonaType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PruebaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nombre')->add('apellido')->add('edad');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PruebaBundle\Entity\Persona'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'pruebabundle_persona';
+    }
+
+
+}
