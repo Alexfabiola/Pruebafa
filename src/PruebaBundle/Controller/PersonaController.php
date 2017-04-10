364b10c94181ad9a32a337c58f2200d93f55ca7f
Fabiola <alexfabiola28@gmail.com>
2017-04-10 19:50:07 -0400

    Falto del Anterior

--- src/PruebaBundle/Controller/PersonaController.php
+++ src/PruebaBundle/Controller/PersonaController.php
@@ -5,7 +5,8 @@ namespace PruebaBundle\Controller;
 use PruebaBundle\Entity\Persona;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Persona controller.
